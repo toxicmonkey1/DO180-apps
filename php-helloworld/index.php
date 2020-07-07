0fefddd19322f1736c2af7357d0209aadf09045a
Student User <student@workstation.lab.example.com>
2020-07-07 20:33:51 +0000

    updated index.html

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "A change in is the air --  'Stand by the grey stone when the thrush knocks, and the setting sun with the last light of Durin's Day will shine upon the key-hole.'\n";
 ?>
