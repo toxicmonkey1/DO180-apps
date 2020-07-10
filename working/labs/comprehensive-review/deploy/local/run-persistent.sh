264384ea13f80ebab1378afcfd735f01db0f02f2
Student User <student@workstation.lab.example.com>
2020-07-10 12:49:46 +0000

    added labs

---
+++ working/labs/comprehensive-review/deploy/local/run-persistent.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+if [ ! -d /tmp/docker/work ]; then
+  mkdir -p /tmp/docker/work
+  sudo semanage fcontext -a -t container_file_t '/tmp/docker/work(/.*)?'
+  sudo restorecon -R /tmp/docker/work
+  sudo chown 1001:1001 /tmp/docker/work
+fi
+
+sudo podman run -d -v /tmp/docker/work:/opt/nexus/sonatype-work nexus
